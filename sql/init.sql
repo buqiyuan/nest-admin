d5825563d3010d0ce5e7ced7b1e28b0421315e8a
烂柯人 <57043221+lankerened@users.noreply.github.com>
2022-06-08 17:03:36 +0800

    删除错误语句

--- sql/init.sql
+++ sql/init.sql
@@ -1,4 +1,3 @@
-mysqldump: [Warning] Using a password on the command line interface can be insecure.
 -- MySQL dump 10.13  Distrib 8.0.26, for Linux (x86_64)
 --
 -- Host: localhost    Database: nest-admin
