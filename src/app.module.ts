11e917cec4e36dbebbd6e49706dcc0ad09c2bbe4
buqiyuan <1743369777@qq.com>
2022-10-03 00:59:40 +0800

    chore: remove redundant config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BullModule } from '@nestjs/bull';
-import Configuration from './config/configuration';
+import { Configuration } from './config/configuration';
 import { AdminModule } from './modules/admin/admin.module';
 import { SharedModule } from './shared/shared.module';
 import { MissionModule } from './mission/mission.module';
@@ -22,7 +22,7 @@ import { LOGGER_MODULE_OPTIONS } from './shared/logger/logger.constants';
     ConfigModule.forRoot({
       isGlobal: true,
       load: [Configuration],
-      envFilePath: ['.env', `.env.${process.env.NODE_ENV}`],
+      envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule, LoggerModule],
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,18 +1,83 @@
-import { merge } from 'lodash';
-import DefaultConfig from './config.default';
-import { IConfig } from './defineConfig';
+import { DataSourceOptions } from 'typeorm';
+import * as qiniu from 'qiniu';
 
-/**
- * 根据环境变量判断使用配置
- */
-export default () => {
-  let envConfig: IConfig = {};
-  try {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    envConfig = require(`./config.${process.env.NODE_ENV}`).default;
-  } catch (e) {
-    // 无效配置则自动忽略
+const parseZone = (zone: string) => {
+  switch (zone) {
+    case 'Zone_as0':
+      return qiniu.zone.Zone_as0;
+    case 'Zone_na0':
+      return qiniu.zone.Zone_na0;
+    case 'Zone_z0':
+      return qiniu.zone.Zone_z0;
+    case 'Zone_z1':
+      return qiniu.zone.Zone_z1;
+    case 'Zone_z2':
+      return qiniu.zone.Zone_z2;
   }
-  // 合并配置
-  return merge(DefaultConfig, envConfig);
 };
+
+export const Configuration = () => ({
+  rootRoleId: parseInt(process.env.ROOT_ROLE_ID || '1'),
+  // nodemailer config
+  mailer: {
+    host: 'xxx',
+    port: 80,
+    auth: {
+      user: 'xxx',
+      pass: 'xxx',
+    },
+    secure: false, // or true using 443
+  },
+  // amap config
+  amap: {
+    key: 'xxx',
+  },
+  // jwt sign secret
+  jwt: {
+    secret: process.env.JWT_SECRET || '123456',
+  },
+  // typeorm config
+  database: {
+    type: 'mysql',
+    host: process.env.MYSQL_HOST,
+    port: parseInt(process.env.MYSQL_PORT, 10),
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    synchronize: true,
+    logging: ['error'],
+    timezone: '+08:00', // 东八区
+  } as DataSourceOptions,
+  redis: {
+    host: process.env.REDIS_HOST, // default value
+    port: parseInt(process.env.REDIS_PORT, 10), // default value
+    password: process.env.REDIS_PASSWORD,
+    db: process.env.REDIS_DB,
+  },
+  // qiniu config
+  qiniu: {
+    accessKey: process.env.QINIU_ACCESSKEY,
+    secretKey: process.env.QINIU_SECRETKEY,
+    domain: process.env.QINIU_DOMAIN,
+    bucket: process.env.QINIU_BUCKET,
+    zone: parseZone(process.env.QINIU_ZONE || 'Zone_z2'),
+    access: (process.env.QINIU_ACCESS_TYPE as any) || 'public',
+  },
+  // logger config
+  logger: {
+    timestamp: false,
+    dir: process.env.LOGGER_DIR,
+    maxFileSize: process.env.LOGGER_MAX_SIZE,
+    maxFiles: process.env.LOGGER_MAX_FILES,
+    errorLogName: process.env.LOGGER_ERROR_FILENAME,
+    appLogName: process.env.LOGGER_APP_FILENAME,
+  },
+  // swagger
+  swagger: {
+    enable: process.env.SWAGGER_ENABLE === 'true',
+    path: process.env.SWAGGER_PATH,
+    title: process.env.SWAGGER_TITLE,
+    desc: process.env.SWAGGER_DESC,
+    version: process.env.SWAGGER_VERSION,
+  },
+});
