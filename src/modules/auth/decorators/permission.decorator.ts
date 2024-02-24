3ec4757e76e0314671b482e8f59d5936fa44842e
buqiyuan <1743369777@qq.com>
2024-02-24 17:46:13 +0800

    fix: definePermission return ncorrect value

--- src/modules/auth/decorators/permission.decorator.ts
+++ src/modules/auth/decorators/permission.decorator.ts
@@ -1,12 +1,14 @@
 import { SetMetadata, applyDecorators } from '@nestjs/common'
 
+import { isPlainObject } from 'lodash'
+
 import { PERMISSION_KEY } from '../auth.constant'
 
  type TupleToObject<T extends string, P extends ReadonlyArray<string>> = {
    [K in Uppercase<P[number]>]: `${T}:${Lowercase<K>}`
  }
  type AddPrefixToObjectValue<T extends string, P extends Record<string, string>> = {
-   [K in keyof P]: K extends string ? `${T}:${K}` : never
+   [K in keyof P]: K extends string ? `${T}:${P[K]}` : never
  }
 
 /** 资源操作需要特定的权限 */
@@ -34,9 +36,11 @@ let permissions: string[] = []
 export function definePermission<T extends string, U extends Record<string, string>>(modulePrefix: T, actionMap: U): AddPrefixToObjectValue<T, U>
 export function definePermission<T extends string, U extends ReadonlyArray<string>>(modulePrefix: T, actions: U): TupleToObject<T, U>
 export function definePermission(modulePrefix: string, actions) {
-  if (typeof actions === 'object') {
-    const permissionFormats = Object.values(actions).map(action => `${modulePrefix}:${action}`)
-    permissions = [...new Set([...permissions, ...permissionFormats])]
+  if (isPlainObject(actions)) {
+    Object.entries(actions).forEach(([key, action]) => {
+      actions[key] = `${modulePrefix}:${action}`
+    })
+    permissions = [...new Set([...permissions, ...Object.values<string>(actions)])]
     return actions
   }
   else if (Array.isArray(actions)) {
