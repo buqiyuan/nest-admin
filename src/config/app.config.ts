c473b73b9e14b5ddbe4f86b79b93bedc7f213d16
June <1601745371@qq.com>
2024-08-10 10:58:22 +0800

    fix: 修复token过期后，获取图片验证码接口依然报1101问题

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -4,11 +4,12 @@ import { env, envBoolean, envNumber } from '~/global/env'
 
 export const appRegToken = 'app'
 
+const globalPrefix = env('GLOBAL_PREFIX', 'api')
 export const AppConfig = registerAs(appRegToken, () => ({
   name: env('APP_NAME'),
   port: envNumber('APP_PORT', 3000),
   baseUrl: env('APP_BASE_URL'),
-  globalPrefix: env('GLOBAL_PREFIX', 'api'),
+  globalPrefix,
   locale: env('APP_LOCALE', 'zh-CN'),
   /** 是否允许多端登录 */
   multiDeviceLogin: envBoolean('MULTI_DEVICE_LOGIN', true),
@@ -20,3 +21,8 @@ export const AppConfig = registerAs(appRegToken, () => ({
 }))
 
 export type IAppConfig = ConfigType<typeof AppConfig>
+
+export const RouterWhiteList: string[] = [
+  `${globalPrefix ? '/' : ''}${globalPrefix}/auth/captcha/img`,
+  `${globalPrefix ? '/' : ''}${globalPrefix}/auth/login`
+]
